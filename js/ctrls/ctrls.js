f29e4c1855b198d32f2b485d542bd470c84ba01c
Gabriel Lucas de Souza <gabrielsouzadevweb@gmail.com>
2020-08-05 12:57:11 -0300

    solucao js completa

--- js/ctrls/ctrls.js
+++ js/ctrls/ctrls.js
@@ -1,12 +1,26 @@
 class Ctrls{
     constructor(){
-       this.toggleWithTime();
+        this._allContainer = document.querySelectorAll(".jsContainer");
+        this._index = 0;
+        this.toggleWithTime();      
     }
 
     toggleWithTime(){
-        let att = document.querySelector(".jsContainer-1");
         setInterval(() => {
-           att.style.zIndex = "-10";
+            this.changeIndex();
         }, 5000);
+    }
+
+    changeIndex(){
+        this.resetAllIndex();
+        this._allContainer[this._index].style.zIndex= `${this._allContainer.length}`;
+        (this._index == this._allContainer.length -1) ? this._index=0: this._index++;
+        console.log(this._index);
+    }
+
+    resetAllIndex(){
+        this._allContainer.forEach(e => {
+            e.style.zIndex= "0";
+        });
     }
 }
